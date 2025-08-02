ff73a69036364dd5d2100aa1367cedff450b5478
LoKiiZoshi <lokendarjoshi384@gmail.com>
2025-08-02 12:40:21 +0545

    update

--- PortfolioLokiiapp/admin.py
+++ PortfolioLokiiapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Person, Skill, Project, Blog, ClientService
 
+
 # Register Person model
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
